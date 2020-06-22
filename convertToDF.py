0e327fe34419d1a28298ccef278709be7a78705c
nmerkel <nmerkel@umd.edu>
2020-06-22 19:57:38 -0400

    updated convertToDF

--- convertToDF.py
+++ convertToDF.py
@@ -2,13 +2,13 @@ import pandas as pd
 import glob
 import json
 import re
+# from same blog post that recommended TweetScraper:
+# https://www.simonlindgren.com/notes/2017/11/7/scrape-tweets-without-using-the-api
 
 def main():
     files = glob.glob('Data/tweet/*')
-    print(len(files))
 
-
-    dictlist = []
+    tweets = []
     for file in files:
         json_string = open(file, 'r').read()
         json_dict = json.loads(json_string)
@@ -19,15 +19,15 @@ def main():
         if text == '': continue # if no text left... ignore that tweet!
 
         url = json_dict['url']
-        cleaned_dict = {
+        cleaned_info= {
             'text' : text,
             'url' : url
         }
-        dictlist.append(cleaned_dict)
+        tweets.append(cleaned_info)
 
 
     # have list of tweets. now want dataframe
-    df = pd.DataFrame(dictlist)
+    df = pd.DataFrame(tweets)
 
     df = df.replace({'\n': ' '}, regex=True) # remove linebreaks in the dataframe
     df = df.replace({'\t': ' '}, regex=True) # remove tabs in the dataframe
